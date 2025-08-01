fceab34b5a22967414e02f23e25c7fdfcdb59c00
BichNhung263 <bichnhung263@gmail.com>
2025-08-01 16:20:41 -0300

    Thêm Register

---
+++ app/src/main/java/com/hothibichnhung/hothibichnhung_2123110314/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.hothibichnhung.hothibichnhung_2123110314;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btnNext = findViewById(R.id.btnLogin);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText objEmail = findViewById(R.id.etEmail);
+                String txtEmail = objEmail.getText().toString();
+
+                EditText objPass = findViewById(R.id.etPassword);
+                String txtPass = objPass.getText().toString();
+
+                CharSequence text = txtEmail + " " + txtPass;
+                int duration = Toast.LENGTH_SHORT;
+                if (txtEmail.equals("BichNhung") && txtPass.equals("263"))
+                {
+                    Intent it =new Intent(getApplicationContext(),RegisterActivity.class);
+                    startActivity(it);
+                }
+                else {
+
+                    Toast toast = Toast.makeText(getApplicationContext(),  "Login fail", duration);
+                    toast.show();
+                }
+            }
+        });
+
+    }
+}
